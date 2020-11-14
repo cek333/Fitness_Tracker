9c0701eddc7469e1eb930d927f0f768fe0ecb507
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-14 12:54:56 -0500

    comment out debug messages

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,30 +11,30 @@ mongoose.connect("mongodb://localhost/workout", {
 
 router.get('/api/workouts', async function(req, res){
   let result = await db.Workout.find({});
-  console.log('[get /api/workouts]', result);
+  //console.log('[get /api/workouts]', result);
   res.json(result);
 });
 
 router.get('/api/workouts/range', async function(req, res){
   let result = await db.Workout.find({}).sort({day: -1}).limit(7).exec();
-  console.log('[get /api/workouts/range]', result);
+  //console.log('[get /api/workouts/range]', result);
   res.json(result);
 });
 
 router.post('/api/workouts', async function(req, res) {
-  console.log('[post /api/workouts]', req.body);
+  //console.log('[post /api/workouts]', req.body);
   // Note: if req.body empty (typical case), no harm done
   let result = await db.Workout.create({ exercise: [ req.body ] });
-  console.log(`[post /api/workouts] insertId=${result.insertId}`, result);
+  //console.log(`[post /api/workouts] insertId=${result.insertId}`, result);
   res.json(result);
 });
 
 router.put('/api/workouts/:id', async function(req, res) {
-  console.log(`[put /api/workouts/:id] id=${req.params.id}`, req.body);
+  //console.log(`[put /api/workouts/:id] id=${req.params.id}`, req.body);
   let workout = await db.Workout.findById(req.params.id);
   workout.exercises.push(req.body);
   let result = await workout.save();
-  console.log(`[post /api/workouts/:id]`, result);
+  //console.log(`[post /api/workouts/:id]`, result);
   res.json(result);
 });
 
